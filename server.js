42ffac56d0826702d7e4518d52961ab5a464c328
mayn_ku <maynku07@gmail.com>
2025-01-13 11:04:45 +0530

    next version

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ app.get('/', function (req, res) {
 
 
 
-
 const personRoutes=require('./routes/personRoutes')
 app.use('/person',personRoutes);
 const menuRoutes=require('./routes/menuRoutes')
 app.use('/menu',menuRoutes);
+//listening 
 
 app.listen(3000,()=>{
     console.log("listening on port 3000");
